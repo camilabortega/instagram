826480c6c52e8de633492129537c702465f11125
Camila <camila.bu.ortega@gmail.com>
2020-07-04 21:07:06 -0300

    Terminando posts JSON

--- js/feed.js
+++ js/feed.js
@@ -34,8 +34,6 @@ setTimeout(function(){
             article.appendChild(header);
             article.appendChild(img);
 
-            feed.appendChild(article);
-
             //Footer do post
             var articleHashtags = document.createElement("span");
             articleHashtags.className = "hashtags";
@@ -51,6 +49,7 @@ setTimeout(function(){
             strongUser.innerText = post.user.name;
 
             var spanPostText = document.createElement("span");
+            spanPostText.className = "post-text";
             spanPostText.innerText = post.post_text;
 
             var p = document.createElement("p");
@@ -62,59 +61,100 @@ setTimeout(function(){
             articleText.className = "article-text";
             articleText.appendChild(p);
 
-            //article.innerHTML = 
-            //     `   
-            //     <footer>
-            //         <div class="padding">
-    
-            //             <div class="article-actions">
-            //                 <div class="user-reactions">
-            //                     <a href="#like"><img id="like01" class="like" src="svg/like.svg" alt="Curtir"></a>
-            //                     <a href="#comment"><img src="svg/comment.svg" alt="Comentar"></a>
-            //                     <a href="#send"><img src="svg/messages.svg" alt="Enviar"></a>
-            //             </div>
-    
-            //             <div class="user-save">
-            //                 <img src="svg/save.svg" alt="Salvar">
-            //             </div>
-            //         </div>
-    
-            //         <div class="likes-count">
-            //             <p>
-            //                 <strong><span id="likes-${post.post_id}">${post.likes}</span> likes</strong>
-            //             </p>
-            //         </div>
-    
-            //         <!-- article-text -->
-    
-            //         <div class="view-comments">
-            //             <p>View all <span>9</span> comments</p>
-            //         </div>
-                    
-            //         <div class="article-comments">
-            //             <p>
-            //                 <strong>outro_usuário</strong> <span>Texto do comentário</span> 
-            //                 <span class="hashtags">
-            //                    <a href="#hashtag1"><span>#hashtag1</span></a>
-            //                    <a href="#hashtag2"><span>#hashtag2</span></a>
-            //                 </span>
-            //             </p>
-            //             <div class="like-comment">
-            //                 <img src="svg/like-comment.svg" alt="Curtir">
-            //             </div>
-            //         </div>
-    
-            //         <div class="article-date">
-            //             <p>1 hour ago</p>
-            //         </div>
-    
-            //         </div>
-            //         <form class="comment">
-            //         <textarea name="comment" id="comment" placeholder="Add a comment..."></textarea>
-            //         <button type="submit"><strong>Post</strong></button>
-            //         </form>
-            //     </footer>`;
-            // feed.appendChild(article);
+            var articleActions = document.createElement("div");
+            articleActions.className = "article-actions";
+            articleActions.innerHTML = `
+            <div class="user-reactions">
+                <a href="#like"><img id="like01" class="like" src="svg/like.svg" alt="Curtir"></a>
+                <a href="#comment"><img src="svg/comment.svg" alt="Comentar"></a>
+                <a href="#send"><img src="svg/messages.svg" alt="Enviar"></a>
+            </div>
+            <div class="user-save">
+                <img src="svg/save.svg" alt="Salvar">
+            </div>
+            `;
+
+            var likesCount = document.createElement("div");
+            likesCount.className = "likes-count";
+            likesCount.innerHTML = `
+            <p>
+                <strong><span id="likes-${post.post_id}">${post.likes}</span> likes</strong>
+            </p>
+            `;
+
+            var viewComments = document.createElement("div");
+            viewComments.className = "view-comments";
+            viewComments.innerHTML = `
+            <p>View all <span>${post.comments.length}</span> comments</p>
+            `;
+
+            var articleComments = document.createElement("div");
+            articleComments.className = "article-comments";
+
+            for(comment of post.comments){
+                var commentDiv = document.createElement("div");
+                commentDiv.className = "comment";
+
+                var spanCommentHashtags = document.createElement("span");
+                spanCommentHashtags.className = "hashtags";
+
+                for(hashtag of comment.comment_hashtags){
+                    var aHashtags = document.createElement("a");
+                    aHashtags.href = hashtag;
+                    aHashtags.innerHTML = `<span>${hashtag}</span>`;
+                    spanCommentHashtags.appendChild(aHashtags);
+                }
+
+                var pComment = document.createElement("p");
+                var strongComment = document.createElement("strong");
+                var spanComment = document.createElement("span");
+                spanComment.className = "comment-text";
+
+                strongComment.innerText = comment.comment_user;
+                spanComment.innerText = comment.comment_text;
+
+                pComment.appendChild(strongComment);
+                pComment.appendChild(spanComment);
+                pComment.appendChild(spanCommentHashtags);
+
+                commentDiv.appendChild(pComment);
+
+                var likeComment = document.createElement("div");
+                likeComment.className = "like-comment";
+                likeComment.innerHTML = `<img src="svg/like-comment.svg" alt="Curtir"/>`;
+
+                commentDiv.appendChild(likeComment);
+                articleComments.appendChild(commentDiv);
+            }
+
+            var articleDate = document.createElement("div");
+            articleDate.className = "article-date";
+            articleDate.innerHTML = `<p>${post.post_date}</p>`;
+
+            var padding = document.createElement("div");
+            padding.className = "padding";
+
+            padding.appendChild(articleActions);
+            padding.appendChild(likesCount);
+            padding.appendChild(articleText);
+            padding.appendChild(viewComments);
+            padding.appendChild(articleComments);
+            padding.appendChild(articleDate);
+
+            var form = document.createElement("form");
+            form.className = "comment";
+            form.innerHTML = `
+            <textarea name="comment" id="comment-${post.post_id}" placeholder="Add a comment..."></textarea>
+            <button type="submit"><strong>Post</strong></button>
+            `;
+
+            var footer = document.createElement("footer");
+            footer.appendChild(padding);
+            footer.appendChild(form);
+
+            article.appendChild(footer);
+
+            feed.appendChild(article);
         }
     })
     .catch(function(error){
